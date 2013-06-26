cc24403f6b5bfb787fde9f7d6b6c5243637147d6
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 14:51:53 -0500

    added authorization and submit to socrates

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,5 @@
+enable :sessions
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
@@ -15,7 +17,7 @@ end
 
 post '/register' do
   puts params.inspect
-  @user = User.new(params)
+  @user = User.new(params[:user])
   if @user.save
     erb :register
   else
@@ -25,10 +27,30 @@ post '/register' do
 end
 
 post '/login' do
-  @user = User.authenticate(params)
-  if @user.nil?
-    erb :login
-  else
-    erb :secret
-  end
+  @user = User.find_by_email(params[:user][:email])
+    if @user.password == params[:user][:password]
+      session[:user] = @user.id
+      redirect('/secret')
+    else
+      erb :login
+    end
+  # @user = User.authenticate(params[:user])
+  # puts params.inspect
+  # if @user.nil?
+  #   erb :login
+  # else
+  #   redirect '/secret'
+  # end
 end
+
+get '/secret' do
+  erb :secret
+end
+
+get '/logout' do
+  session[:user] = nil
+  redirect '/'
+end
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,29 @@
 class User < ActiveRecord::Base
+  include BCrypt
+
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :email, :presence => true, :uniqueness => true
-  validates :password, :presence => true
+  validates :password_hash, :presence => true
   validates_format_of :email, :with => /^\S+@[a-zA-Z0-9\-]+\.[a-z]{2,}$/
 
   def self.authenticate(args)
+    args[:password] = BCrypt::Password.create(args[:password])
     User.find_by_email_and_password(args[:email], args[:password])
   end
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  # def self.build
+  #   @user = User.new(params[:user])
+  #   @user.password = params[:password]
+  #   return @user
+  # end
 end
